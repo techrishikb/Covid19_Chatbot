d02828ac5d6d40f445d66ed920b2c9ba61ca9ffc
techrishikb <68335914+techrishikb@users.noreply.github.com>
2020-07-29 02:43:02 +0530

    Update actions.py

--- actions.py
+++ actions.py
@@ -14,20 +14,7 @@ from rasa_sdk.executor import CollectingDispatcher
 import requests
 
 
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+
 
 class ActionHelloWorld(Action):
     def name(self) -> Text:
@@ -40,27 +27,7 @@ class ActionHelloWorld(Action):
 
         return []
 		
-class ActionSearchRestaurant(Action):
-    def name(self) -> Text:
-        return "action_search_restaurant"
 
-    def run(self, dispatcher: CollectingDispatcher,
-             tracker: Tracker,
-             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        entities = tracker.latest_message['entities']
-        print(entities)
-        
-        for e in entities:
-            if e['entity'] =='hotel':
-                name = e['value']
-                
-            if name =="indian":
-                message = "Indian1, Indian2, Indian3, Indian4, Indian5"
-            if name =="chinese":
-                message = "chinese1, chinese2, chinese3, chinese4, chinese5"
-        dispatcher.utter_message(text="Hello World from restaurant search")
-
-        return []
 		
 
 class ActionCoronaTracker(Action):
